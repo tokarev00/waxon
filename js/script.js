a1b9263e32f7c2b4a08872241491dfad1754fc4a
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2020-07-11 02:15:59 +0300

    added jquery and SLICK SLIDER
    added  jquery and SLICK SLIDER, cleared vertical scroll(added overflow-x hidden for bady)

--- js/script.js
+++ js/script.js
@@ -1,33 +1,7 @@
-"use strict";
-let sliderViewSlider = document.getElementById("slider__view-slider").offsetWidth;
-let btnNext = document.getElementById("slider__btn_next");
-let btnPrev = document.getElementById("slider__btn_prev");
-let sliderWrapper = document.querySelector("div.slider__wrapper");
-let viewSliders = document.querySelectorAll(".slider__view-point");
-let viewSlide = 0;
-viewSliders[0].style.opacity = "0";
-viewSliders[0].style.border = "solid 1px white"
-btnNext.addEventListener("click", function () {
-    viewSliders[viewSlide].style.opacity = ".6";
-    if (viewSlide < 4) {
-        viewSlide++;
-    } else {
-        viewSlide = 0;
-    }
-    viewSliders[viewSlide].style.opacity = "0";
-     viewSliders[viewSlide].style.border = "solid 1px white"
-    sliderWrapper.style.left = -viewSlide * sliderViewSlider + "px";
-});
- 
-
-btnPrev.addEventListener("click", function () {
-    viewSliders[viewSlide].style.opacity = ".6";
-    if (viewSlide > 0) {
-        viewSlide--; 
-    } else {
-        viewSlide = 4; 
-    }
-    viewSliders[viewSlide].style.opacity = "0";
-    viewSliders[viewSlide].style.border = "solid 1px white";
-    sliderWrapper.style.left = -viewSlide * sliderViewSlider + "px";
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows: true,
+		dots:true,
+		adaptiveHeight:true
+		});
 });
